8ea58ab1e25fd530c0a30cf1b5fc396c4907adcb
Illya <katinskyi.illia@lll.kpi.ua>
2024-12-23 00:23:06 +0200

    Remove error exclamation point

--- services/UserService.js
+++ services/UserService.js
@@ -14,7 +14,7 @@ class UserService {
     if (!password) {
       throw new ApiError(400, 'Password is required');
     }
-    if (!password.length < 8) {
+    if (password.length < 8) {
       throw new ApiError(400, 'Password must be at least 8 characters');
     }
     if (!/\d/.test(password)) {
