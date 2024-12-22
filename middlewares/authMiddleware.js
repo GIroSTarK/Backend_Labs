b1167b2f195b8b2866f149493cc81d4a5298aea8
Illya <katinskyi.illia@lll.kpi.ua>
2024-12-22 20:32:20 +0200

    Add auth middleware

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+
+export const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (!token) {
+    return res.status(401).json({ error: 'Token is required' });
+  }
+
+  try {
+    const user = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = user;
+    next();
+  } catch (err) {
+    res.status(403).json({ error: 'Invalid token' });
+  }
+};
